294e8672a01052036d780b49fcc33b24aed89ef8
heysivani <heysivani@gmail.com>
2020-10-02 15:18:15 +0900

    Wrote basic structure for BinarySearchTree insert
    Co-authored-by: heysivani <heysivani@users.noreply.github.com>
    Co-authored-by: dhequex <dhequex@users.noreply.github.com>

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,4 +1,41 @@
-class BinarySearchTree {}
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    let child = new BinarySearchTree(value);
+    if (value > this.value) {
+      this.right = child;
+    }
+    if (value < this.value) {
+      this.left = child;
+    }
+
+    return child;
+  }
+
+  contains(value) {
+    let flag = false;
+
+    let finder = (tree) => {
+      if (tree.value === value) {
+        flag = true;
+        return;
+      }
+      if (tree.children.length !== 0) {
+        for (let subTree of tree.children) {
+          finder(subTree);
+        }
+      }
+      return;
+    };
+    finder(this);
+    return flag;
+  }
+}
 
 /*
 |XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX

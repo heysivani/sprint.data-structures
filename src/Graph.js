72a6b88b5c0966665abddaf520c0c002622f0112
heysivani <heysivani@gmail.com>
2020-10-03 22:08:19 +0900

    Graph methods pass some tests
    Co-authored-by: heysivani <heysivani@users.noreply.github.com>
    Co-authored-by: dhequex <dhequex@users.noreply.github.com>

--- src/Graph.js
+++ src/Graph.js
@@ -1,13 +1,42 @@
 class Graph {
-  constructor() {
-    /**
-     * There are many representations of Graph. 
-     * We can store Graph in many ways. 
-     * One way is by using Adjacency List. 
-     */
-    this.nodes = {};  // Adjacency List
+  constructor(value) {
+    this.value = value;
+    this.nodes = {}; // Adjacency List
   }
+
+  addNode(value) {
+    this.nodes[value] = [];
+    return undefined;
+  }
+
+  removeNode(value) {
+    this.nodes[value] = undefined;
+    return undefined;
+  }
+
+  contains(value) {
+    if (this.nodes[value] !== undefined) {
+      return true;
+    }
+    return false;
+  }
+
+  addEdge(v1, v2) {
+    if (this.contains(v1) && this.contains(v2)) {
+      if (!this.nodes[v1].includes(v2)) {
+        this.nodes[v1].push(v2);
+      }
+      if (!this.nodes[v2].includes(v1)) {
+        this.nodes[v2].push(v1);
+      }
+      return undefined;
+    }
+
+    return "Invalid node value";
+  }
+  hasEdge() {}
 }
+module.exports = Graph;
 
 /*
 |XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX

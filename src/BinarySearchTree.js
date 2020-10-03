82aa99401ad4c25cc91c45a7b3f3ca660d096c42
heysivani <heysivani@gmail.com>
2020-10-03 17:00:10 +0900

    BinarySearchTree insert method passes all tests
    Co-authored-by: dhequex <dhequex@users.noreply.github.com>
    Co-authored-by: heysivani <heysivani@users.noreply.github.com>

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -6,16 +6,44 @@ class BinarySearchTree {
   }
 
   insert(value) {
-    let child = new BinarySearchTree(value);
-    if (value > this.value) {
-      this.right = child;
-    }
-    if (value < this.value) {
-      this.left = child;
-    }
+    let newNode = new BinarySearchTree(value);
 
+    let recurser = (tree) => {
+      if (value < tree.value) {
+        // if value is smaller, check if left is null
+        if (tree.left !== null) {
+          recurser(tree.left);
+        } else {
+          // if no left, insert new node here
+          tree.left = newNode;
+          return;
+        }
+      } else if (value > tree.value) {
+        // if value is bigger, check if right is null
+        if (tree.right !== null) {
+          recurser(tree.right);
+        } else {
+          // if no right, insert new node here
+          tree.right = newNode;
+          return;
+        }
+      }
+    };
+
+    recurser(this);
     return this;
   }
+  // insert(value) {
+  //   let child = new BinarySearchTree(value);
+  //   if (value > this.value) {
+  //     this.right = child;
+  //   }
+  //   if (value < this.value) {
+  //     this.left = child;
+  //   }
+
+  //   return this;
+  // }
 
   contains(value) {
     let flag = false;
